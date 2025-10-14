9a6f3a3bea968a9bb9a8c85c89e8f4b5aa6a9d67
Anuja Rawat <anujarawat606@gmail.com>
2025-10-15 00:01:29 +0200

    Create marketing_traffic_kpis.sql
    adding a new file for traffic KPIs

---
+++ marketing_metrics/marketing_traffic_kpis.sql
@@ -0,0 +1,12 @@
+SELECT 
+  YEAR,
+  ISOWEEK,
+  sum(users) as total_users,
+  sum(sessions) as total_sessions,
+  sum(orders) as total_orders,
+  round((sum(sessions) / sum(users)), 0) as sessions_per_user,
+  round((sum(orders) / sum(users)), 2) as orders_per_user,
+  round((sum(orders) / sum(sessions)), 2) as conversion_ratio
+FROM default.lieferando_traffic
+GROUP BY ALL
+ORDER BY year, isoweek
